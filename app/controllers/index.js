9b299cc0e1e5b47b132decded0e856da95d55647
bertrand <bpinel@ippon.fr>
2015-08-21 12:09:10 +0200

    Strava request displayed in Ember Model-table component

---
+++ app/controllers/index.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  bikeColumns: Ember.computed(function() {
+    var col = Ember.A([
+      Ember.Object.create({
+        propertyName: 'name',
+        title: 'Name'
+      }),
+      Ember.Object.create({
+        propertyName: 'distance',
+        title: 'Distance'
+      }),
+      Ember.Object.create({
+        propertyName: 'principal',
+         title: 'Primary'
+      })
+    ]);
+    return col;
+  }),
+
+  bikeContent: Ember.computed(function() {
+    return this.get("model").bikes;
+  }),
+
+  shoeColumns: Ember.computed(function() {
+    var col = Ember.A([
+      Ember.Object.create({
+        propertyName: 'name',
+        title: 'Name'
+      }),
+      Ember.Object.create({
+        propertyName: 'distance',
+        title: 'Distance'
+      }),
+      Ember.Object.create({
+        propertyName: 'principal',
+         title: 'Primary'
+      })
+    ]);
+    return col;
+  }),
+
+  shoeContent: Ember.computed(function() {
+    return this.get("model").shoes;
+  }),
+
+
+});
