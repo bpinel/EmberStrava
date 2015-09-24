5b82438667e60a928ba9e7342d0ef2372cdc0bc6
Bertrand Pinel <bpinel@ippon.fr>
2015-09-24 15:28:07 +0200

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function() {	
-       let token = "cf7369d2f867efc4ed9ed2feb45c609656ed4642";
+       let token = "YOUR TOKEN STRAVA";
 	  	return Ember.$.ajax({
   			url: "https://www.strava.com/api/v3/athlete?access_token="+token+"&callback=?",
   			dataType: "jsonp", 
